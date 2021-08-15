989c7453fed1dea0f44b98a45a419241d16b6238
MistyRain <64189704+yaoxiaonie@users.noreply.github.com>
2021-08-15 11:01:35 +0800

    Fix bugs and optimize rootfs settings
    Fix:
    1. ping permission missing
    2. can not get online
    Optimize:
    1. remove unavailable partitions
    2. add sshd service
    3. exec rootfs

--- asl.sh
+++ asl.sh
@@ -2,9 +2,9 @@
 
 # Copyright (C) 2021 MistyRain <1740621736@qq.com>
 
-ROOTFS_PARTITIONS="/ /dev /proc /sys /dev/shm /dev/pts"
-ANDROID_PARTITIONS="/dev/fd /dev/stdin /dev/stout /dev/sterr /dev/tty0 /dev/net/tun"
-ASL_VERSION="Dev-2.0"
+MISSING_PARTITIONS="/dev /dev/pts /dev/net /sys /sys/virtual/socket /proc"
+ROOTFS_PARTITIONS="/dev /dev/pts /proc"
+ASL_VERSION="Dev-2.1"
 
 function ASL_PRINT() {
     echo "「$(date '+%Y-%m-%d %H:%M:%S')」"$*""
@@ -14,22 +14,42 @@ function INSTALL_LINUX() {
     if [ -f "$1" ]; then
         TARGET_LINUX="$1"
         LINUX_TYPE="$2"
-        if [ ! -d "$HOME/$LINUX_TYPE" ]; then
-            mkdir -p "$HOME/$LINUX_TYPE"
+        ROOTFS="$HOME/$LINUX_TYPE"
+        if [ ! -d "$ROOTFS" ]; then
+            mkdir -p "$ROOTFS"
             ASL_PRINT "正在释放容器，请稍候..."
-            tar -xJpf "$THISPATH/$TARGET_LINUX" -C "$HOME/$LINUX_TYPE" --exclude='dev'
+            tar -xJpf "$THISPATH/$TARGET_LINUX" -C "$ROOTFS" --exclude='dev'
             ASL_PRINT "正在优化系统设置..."
-            echo "127.0.0.1 localhost" > "$HOME/$LINUX_TYPE/etc/hosts"
-            rm -rf "$HOME/$LINUX_TYPE/etc/resolv.conf"
-            echo "nameserver 114.114.114.114" > "$HOME/$LINUX_TYPE/etc/resolv.conf"
-            echo "nameserver 8.8.4.4" >> "$HOME/$LINUX_TYPE/etc/resolv.conf"
-            echo "export  TZ='Asia/Shanghai'" >> "$HOME/$LINUX_TYPE/root/.bashrc"
-            echo "LittleRain" > "$HOME/$LINUX_TYPE/etc/hostname"
-            cp -frp "$HOME/$LINUX_TYPE/etc/apt/sources.list" "$HOME/$LINUX_TYPE/etc/apt/sources.list.bak"
-            sed -i "s|http://ports.ubuntu.com|https://mirrors.tuna.tsinghua.edu.cn|g" "$HOME/$LINUX_TYPE/etc/apt/sources.list"
-            if [ "$LINUX_TYPE" = "ubuntu" ]; then
-                touch "$HOME/$LINUX_TYPE/root/.hushlogin"
+            rm -rf "$ROOTFS/etc/mtab"
+            cp "/proc/mounts" "$ROOTFS/etc/mtab"
+            if ! $(grep -q "^127.0.0.1" "$ROOTFS/etc/hosts"); then
+                echo '127.0.0.1 localhost' >> "$ROOTFS/etc/hosts"
+                sed -i 's/LXC_NAME/LittleRain/g' "$ROOTFS/etc/hosts"
             fi
+            rm -rf "$ROOTFS/etc/resolv.conf"
+            echo "nameserver 8.8.8.8" >> "$ROOTFS/etc/resolv.conf"
+            if [ -f "$ROOTFS/etc/nsswitch.conf" ]; then
+                sed -i 's/systemd//g' "$ROOTFS/etc/nsswitch.conf"
+            fi
+            echo "inet:x:3003:root" >>$ROOTFS/etc/group
+            echo "net_raw:x:3004:root" $ROOTFS/etc/group
+            echo "LittleRain" > "$ROOTFS/etc/hostname"
+            rm -rf "$ROOTFS/etc/localtime"
+            cp -frp "$ROOTFS/usr/share/zoneinfo/Asia/Shanghai" "$ROOTFS/etc/localtime"
+            echo "Asia/Shanghai" > "$rootfs/etc/timezone"
+            cp "$ROOTFS/etc/apt/sources.list" "$ROOTFS/etc/apt/sources.list.bak"
+            sed -i "s|http://ports.ubuntu.com|https://mirrors.tuna.tsinghua.edu.cn|g" "$ROOTFS/etc/apt/sources.list"
+            if [ "$LINUX_TYPE" = "ubuntu" ]; then
+                echo "Debug::NoDropPrivs true;" > "$ROOTFS/etc/apt/apt.conf.d/00no-drop-privs"
+                touch "$ROOTFS/root/.hushlogin"
+            fi
+            EXEC_ROOTFS "echo "root:root" | chpasswd"
+            EXEC_ROOTFS "apt update -y && apt upgrade -y && apt install openssh-server -y"
+            sed -i -E 's/#?Port .*/Port 4022/g' "$ROOTFS/etc/ssh/sshd_config"
+            sed -i -E 's/#?PasswordAuthentication .*/PasswordAuthentication yes/g' "$ROOTFS/etc/ssh/sshd_config"
+            sed -i -E 's/#?PermitRootLogin .*/PermitRootLogin yes/g' "$ROOTFS/etc/ssh/sshd_config"
+            sed -i -E 's/#?AcceptEnv .*/AcceptEnv LANG/g' "$ROOTFS/etc/ssh/sshd_config"
+            EXEC_ROOTFS "/etc/init.d/ssh restart"
             sleep 1
             ASL_PRINT "安装完成！"
         else
@@ -38,6 +58,7 @@ function INSTALL_LINUX() {
     else
         LINUX_TYPE="$1"
         LINUX_VERSION="$2"
+        ROOTFS="$HOME/$LINUX_TYPE"
         ASL_PRINT "正在解析下载链接..."
         ROOTFS_URL=$(curl -sL "https://mirrors.tuna.tsinghua.edu.cn/lxc-images/streams/v1/images.json" | awk -F '[,"}]' '{for(i=1;i<=NF;i++){print $i}}' | grep "images/$LINUX_TYPE/" | grep "$LINUX_VERSION" | grep "/arm64/default/" | grep "rootfs.tar.xz" | awk 'END {print}')
         ASL_PRINT "正在下载 ${LINUX_TYPE} ${LINUX_VERSION} ..."
@@ -46,24 +67,44 @@ function INSTALL_LINUX() {
             ASL_PRINT "下载失败 !"
             exit 1
         fi
-        if [ -d "$HOME/$LINUX_TYPE" ]; then
-            rm -rf "$HOME/$LINUX_TYPE"
+        if [ -d "$ROOTFS" ]; then
+            rm -rf "$ROOTFS"
         fi
-        mkdir -p "$HOME/$LINUX_TYPE"
+        mkdir -p "$ROOTFS"
         ASL_PRINT "正在释放容器，请稍候..."
-        tar -xJpf "$HOME/${LINUX_TYPE}.tar.xz" -C "$HOME/$LINUX_TYPE" --exclude='dev' --exclude='etc/rc.d' --exclude='usr/lib64/pm-utils'
+        tar -xJpf "$THISPATH/$TARGET_LINUX" -C "$ROOTFS" --exclude='dev'
         ASL_PRINT "正在优化系统设置..."
-        echo "127.0.0.1 localhost" > "$HOME/$LINUX_TYPE/etc/hosts"
-        rm -rf "$HOME/$LINUX_TYPE/etc/resolv.conf"
-        echo "nameserver 114.114.114.114" > "$HOME/$LINUX_TYPE/etc/resolv.conf"
-        echo "nameserver 8.8.4.4" >> "$HOME/$LINUX_TYPE/etc/resolv.conf"
-        echo "export  TZ='Asia/Shanghai'" >> "$HOME/$LINUX_TYPE/root/.bashrc"
-        echo "LittleRain" > "$HOME/$LINUX_TYPE/etc/hostname"
-        cp -frp "$HOME/$LINUX_TYPE/etc/apt/sources.list" "$HOME/$LINUX_TYPE/etc/apt/sources.list.bak"
-        sed -i "s|http://ports.ubuntu.com|https://mirrors.tuna.tsinghua.edu.cn|g" "$HOME/$LINUX_TYPE/etc/apt/sources.list"
-        if [ "$LINUX_TYPE" = "ubuntu" ]; then
-            touch "$HOME/$LINUX_TYPE/root/.hushlogin"
+        rm -rf "$ROOTFS/etc/mtab"
+        cp "/proc/mounts" "$ROOTFS/etc/mtab"
+        if ! $(grep -q "^127.0.0.1" "$ROOTFS/etc/hosts"); then
+            echo '127.0.0.1 localhost' >> "$ROOTFS/etc/hosts"
+            sed -i 's/LXC_NAME/LittleRain/g' "$ROOTFS/etc/hosts"
         fi
+        rm -rf "$ROOTFS/etc/resolv.conf"
+        echo "nameserver 8.8.8.8" >> "$ROOTFS/etc/resolv.conf"
+        if [ -f "$ROOTFS/etc/nsswitch.conf" ]; then
+            sed -i 's/systemd//g' "$ROOTFS/etc/nsswitch.conf"
+        fi
+        echo "inet:x:3003:root" >>$ROOTFS/etc/group
+        echo "net_raw:x:3004:root" $ROOTFS/etc/group
+        echo "LittleRain" > "$ROOTFS/etc/hostname"
+        rm -rf "$ROOTFS/etc/localtime"
+        cp -frp "$ROOTFS/usr/share/zoneinfo/Asia/Shanghai" "$ROOTFS/etc/localtime"
+        echo "Asia/Shanghai" > "$rootfs/etc/timezone"
+        cp "$ROOTFS/etc/apt/sources.list" "$ROOTFS/etc/apt/sources.list.bak"
+        sed -i "s|http://ports.ubuntu.com|https://mirrors.tuna.tsinghua.edu.cn|g" "$ROOTFS/etc/apt/sources.list"
+        if [ "$LINUX_TYPE" = "ubuntu" ]; then
+            echo "Debug::NoDropPrivs true;" > "$ROOTFS/etc/apt/apt.conf.d/00no-drop-privs"
+            touch "$ROOTFS/root/.hushlogin"
+        fi
+        EXEC_ROOTFS "echo "root:root" | chpasswd"
+        EXEC_ROOTFS "apt update -y && apt upgrade -y && apt install openssh-server -y"
+        sed -i -E 's/#?Port .*/Port 4022/g' "$ROOTFS/etc/ssh/sshd_config"
+        sed -i -E 's/#?PasswordAuthentication .*/PasswordAuthentication yes/g' "$ROOTFS/etc/ssh/sshd_config"
+        sed -i -E 's/#?PermitRootLogin .*/PermitRootLogin yes/g' "$ROOTFS/etc/ssh/sshd_config"
+        sed -i -E 's/#?AcceptEnv .*/AcceptEnv LANG/g' "$ROOTFS/etc/ssh/sshd_config"
+        EXEC_ROOTFS "/etc/init.d/ssh restart"
+        sleep 1
         rm -rf "$HOME/${LINUX_TYPE}.tar.xz"
         sleep 1
         echo "安装完成！"
@@ -79,18 +120,54 @@ function MOUNT_STATUS() {
     fi
 }
 
+function CREATE_PARTITIONS() {
+    for TARGET_DIR in $MISSING_PARTITIONS; do
+        if [ ! -d "$ROOTFS${TARGET_DIR}" ]; then
+            case ${TARGET_DIR} in
+            /dev)
+                mkdir -p "$ROOTFS/dev"
+                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
+                ;;
+            /dev/pts)
+                mkdir -p "$ROOTFS/dev/pts"
+                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
+                ;;
+            /dev/net)
+                mkdir -p "$ROOTFS/dev/net"
+                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
+                ;;
+            /sys)
+                mkdir -p "$ROOTFS/sys"
+                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
+                ;;
+            /sys/virtual/socket)
+                mkdir -p "$ROOTFS/sys/virtual/socket"
+                echo "# Dummy File" >"$ROOTFS/sys/virtual/socket/dotest"
+                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
+                ;;
+            /proc)
+                mkdir -p "$ROOTFS/proc"
+                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
+                ;;
+            esac
+        else
+            ASL_PRINT "${TARGET_DIR} 已存在，本次跳过！"
+        fi
+    done
+}
+
 function MOUNT_PARTITIONS() {
     for TARGET_DIR in $ROOTFS_PARTITIONS; do
         if ! MOUNT_STATUS "$ROOTFS${TARGET_DIR}"; then
             case ${TARGET_DIR} in
-            /)
-                mount --rbind $ROOTFS $ROOTFS/
-                mount -o remount,exec,suid,dev "$ROOTFS"
+            /dev)
+                mount -o bind /dev "$ROOTFS/dev"
                 ASL_PRINT "挂载 ${TARGET_DIR} 完成！"
                 ;;
-            /dev)
-                [ -d "$ROOTFS/dev" ] || mkdir -p "$ROOTFS/dev"
-                mount -o bind /dev "$ROOTFS/dev"
+            /dev/pts)
+                [ -d "/dev/pts" ] || mkdir -p /dev/pts
+                mount -o rw,nosuid,noexec,gid=5,mode=620,ptmxmode=000 -t devpts devpts /dev/pts
+                mount -t devpts devpts "$ROOTFS/dev/pts"
                 ASL_PRINT "挂载 ${TARGET_DIR} 完成！"
                 ;;
             /proc)
@@ -98,64 +175,9 @@ function MOUNT_PARTITIONS() {
                 mount -t proc proc "$ROOTFS/proc"
                 ASL_PRINT "挂载 ${TARGET_DIR} 完成！"
                 ;;
-            /sys)
-                [ -d "$ROOTFS/sys" ] || mkdir -p "$ROOTFS/sys"
-                mount -t sysfs sys "$ROOTFS/sys"
-                ASL_PRINT "挂载 ${TARGET_DIR} 完成！"
-                ;;
-            /dev/shm)
-                [ -d "/dev/shm" ] || mkdir -p /dev/shm
-                mount -o rw,nosuid,nodev,mode=1777 -t tmpfs tmpfs /dev/shm
-                [ -d "$ROOTFS/dev/shm" ] || mkdir -p $ROOTFS/dev/shm
-                mount -o bind /dev/shm "$ROOTFS/dev/shm"
-                ASL_PRINT "挂载 ${TARGET_DIR} 完成！"
-                ;;
-            /dev/pts)
-                [ -d "/dev/pts" ] || mkdir -p /dev/pts
-                mount -o rw,nosuid,noexec,gid=5,mode=620,ptmxmode=000 -t devpts devpts /dev/pts
-                [ -d "$ROOTFS/dev/pts" ] || mkdir -p $ROOTFS/dev/pts
-                mount -o bind /dev/pts "$ROOTFS/dev/pts"
-                ASL_PRINT "挂载 ${TARGET_DIR} 完成！"
-                ;;
             esac
         else
             ASL_PRINT "${TARGET_DIR} 已挂载，本次跳过。"
-        fi
-    done
-}
-
-function CONNECT_PARTITIONS() {
-    for TARGET_DIR in $ANDROID_PARTITIONS; do
-        if [ ! -e ${TARGET_DIR} ] && [ ! -h ${TARGET_DIR} ]; then
-            case ${TARGET_DIR} in
-            /dev/fd)
-                ln -s /proc/self/fd ${TARGET_DIR} >/dev/null 2>&1
-                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
-                ;;
-            /dev/stdin)
-                ln -s /proc/self/fd/0 ${TARGET_DIR} >/dev/null 2>&1
-                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
-                ;;
-            /dev/stdout)
-                ln -s /proc/self/fd/1 ${TARGET_DIR} >/dev/null 2>&1
-                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
-                ;;
-            /dev/stderr)
-                ln -s /proc/self/fd/2 ${TARGET_DIR} >/dev/null 2>&1
-                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
-                ;;
-            /dev/tty0)
-                ln -s /dev/null ${TARGET_DIR} >/dev/null 2>&1
-                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
-                ;;
-            /dev/net/tun)
-                [ -d "/dev/net" ] || mkdir -p /dev/net
-                mknod /dev/net/tun c 10 200
-                ASL_PRINT "创建 ${TARGET_DIR} 完成！"
-                ;;
-            esac
-        else
-            ASL_PRINT "${TARGET_DIR} 已存在，本次跳过！"
         fi
     done
 }
@@ -170,12 +192,12 @@ function UMOUNT_PARTITIONS() {
 
 function EXEC_ROOTFS() {
     ASL_PRINT "正在检查容器分区..."
+    CREATE_PARTITIONS
     MOUNT_PARTITIONS
-    CONNECT_PARTITIONS
     echo
     unset TMP TEMP TMPDIR LD_PRELOAD LD_DEBUG
-    PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-    $TOOLKIT/bin/unshare -R "$ROOTFS" /bin/bash -c "$*"
+    PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games:/usr/local/sbin:/sbin
+    $TOOLKIT/bin/unshare -R "$ROOTFS" bash -c "$*"
 }
 
 function USAGE() {
@@ -196,7 +218,8 @@ case "$1" in
     ;;
 -l|--login)
     ROOTFS="$2"
-    EXEC_ROOTFS "/bin/bash --login"
+    EXEC_ROOTFS "bash --login"
+    EXEC_ROOTFS "/etc/init.d/ssh start"
     ;;
 -d|--delete)
     ROOTFS="$2"
