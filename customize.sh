65470d27c8367505b658250dd3b3a70eee1294b8
yaoxiaonie <1740621736@qq.com>
2022-02-06 19:17:32 +0800

    Fix use asl command right

--- customize.sh
+++ customize.sh
@@ -6,13 +6,17 @@ on_install() {
         ui_print "- 正在更新模块..."
         unzip -o "$ZIPFILE" 'asl/*' -d "/data" >&2
         chmod -R 0755 "$ASL/bin"
+        rm -rf /data/adb/modules/asl/system/bin/.empty
+        rm -rf /data/adb/modules/asl/system/xbin/.empty
     else
         ui_print "- 正在释放模块文件..."
         mkdir -p "$ASL"
         unzip -o "$ZIPFILE" 'asl/*' -d "/data" >&2
         chmod -R 0755 "$ASL/bin"
-        ln -s "$ASL/bin/asl.sh" "/system/bin/asl"
-        ln -s "$ASL/bin/asl.sh" "/system/xbin/asl"
+        ln -s "$ASL/bin/asl.sh" "/data/adb/modules/asl/system/bin/asl"
+        ln -s "$ASL/bin/asl.sh" "/data/adb/modules/asl/system/xbin/asl"
+        rm -rf /data/adb/modules/asl/system/bin/.empty
+        rm -rf /data/adb/modules/asl/system/xbin/.empty
         touch "$ASL/rootfs.config"
     fi
 }
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,3 @@
 #此脚本在卸载时进行
 
-rm -rf /system/bin/asl
-rm -rf /system/xbin/asl
 rm -rf /data/asl
